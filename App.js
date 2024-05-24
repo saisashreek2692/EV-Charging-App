14be0d3d6233d1c45da0a71b8e4540c21b978700
saisashreek <saisashreekd@gmail.com>
2024-05-24 17:58:10 +0530

    Added Location Package

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
+import React, { useState, useEffect, useCallback } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
-import { useCallback } from "react";
 import * as Network from "expo-network";
 import LoginScreen from "./App/Screen/LoginScreen";
 import * as SecureStore from "expo-secure-store";
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native";
 import TabNavigation from "./App/Navigation/TabNavigation";
+import * as Location from "expo-location";
+import { UserLocationContext } from "./App/Context/UserLocationContext"
 
 SplashScreen.preventAutoHideAsync();
 
@@ -50,11 +52,37 @@ export default function App() {
     return null;
   }
 
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location.coords);
+    })();
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <ClerkProvider
-    tokenCache={tokenCache}
+      tokenCache={tokenCache}
       publishableKey={`pk_test_cHJlY2lvdXMtaGFnZmlzaC0xMS5jbGVyay5hY2NvdW50cy5kZXYk`}
     >
+      <UserLocationContext.Provider value={{
+        location, setLocation
+      }}>
       <View style={styles.container} onLayout={onLayoutRootView}>
         <SignedIn>
           <NavigationContainer>
@@ -66,6 +94,7 @@ export default function App() {
         </SignedOut>
         <StatusBar style="auto" />
       </View>
+      </UserLocationContext.Provider>
     </ClerkProvider>
   );
 }
