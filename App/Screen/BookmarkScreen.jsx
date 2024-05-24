9cf572de042d78b5f586d16efd3494e47f8785b9
saisashreek <saisashreekd@gmail.com>
2024-05-24 16:15:33 +0530

    Bookmark Screen

---
+++ App/Screen/BookmarkScreen.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function BookmarkScreen() {
+  return (
+    <View>
+      <Text>BookmarkScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
