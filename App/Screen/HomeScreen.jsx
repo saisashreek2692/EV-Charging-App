b1d6a51b5768fbbd51c87008458b3330003c5634
Sai Sashreek D <saisashreekd@gmail.com>
2024-05-27 14:33:48 +0530

    Updated with Header Component

--- App/Screen/HomeScreen.jsx
+++ App/Screen/HomeScreen.jsx
@@ -1,13 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import AppMapView from './AppMapView'
+import HeaderScreen from './HeaderScreen'
 
 export default function HomeScreen() {
   return (
     <View>
+      <View style={styles.headerContainer}>
+        <HeaderScreen />
+      </View>
       <AppMapView />
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  headerContainer: {
+    position: 'absolute',
+    zIndex: 10,
+    width: '100%',
+    padding: 10,
+    marginHorizontal: 20
+  }
+})
