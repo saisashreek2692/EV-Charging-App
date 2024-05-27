6e2f7d89f28463372cb0c72a8b636a0acf7e35de
Sai Sashreek D <saisashreekd@gmail.com>
2024-05-27 14:33:04 +0530

    Added Custom Marker Point

--- App/Screen/AppMapView.jsx
+++ App/Screen/AppMapView.jsx
@@ -1,28 +1,41 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Image } from "react-native";
 import React, { useContext } from "react";
-import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
+import MapView, { Marker, PROVIDER_DEFAULT } from "react-native-maps";
 import MapViewStyle from "../Utils/MapViewStyle.json";
-import { UserLocationContext } from "../Context/UserLocationContext"
+import { UserLocationContext } from "../Context/UserLocationContext";
 
 export default function AppMapView() {
+  const { location, setLocation } = useContext(UserLocationContext);
 
-  const {location, setLocation} = useContext(UserLocationContext)
-
-  return location?.latitude && (
-    <View>
-      <MapView
-        style={styles.map}
-        provider={PROVIDER_DEFAULT}
-        showsUserLocation="true"
-        customMapStyle={MapViewStyle}
-        region={{
-          latitude: location?.latitude,
-          longitude: location?.longitude,
-          latitudeDelta: 0.0422,
-          longitudeDelta: 0.0421
-        }}
-      />
-    </View>
+  return (
+    location?.latitude && (
+      <View>
+        <MapView
+          style={styles.map}
+          provider={PROVIDER_DEFAULT}
+          showsUserLocation="true"
+          customMapStyle={MapViewStyle}
+          region={{
+            latitude: location?.latitude,
+            longitude: location?.longitude,
+            latitudeDelta: 0.0422,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: location?.latitude,
+              longitude: location?.longitude,
+            }}
+          >
+            <Image
+              source={require("../../assets/images/car-marker.png")}
+              style={{ width: 60, height: 60 }}
+            />
+          </Marker>
+        </MapView>
+      </View>
+    )
   );
 }
 
